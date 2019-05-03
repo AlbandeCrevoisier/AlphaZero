03f3a6fa5335a3ccaa8859ad7a4814ca8bd8fc29
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-05-03 17:00:56 +0200

    Wrap the model in a function with comments.

--- alphazero.py
+++ alphazero.py
@@ -6,43 +6,50 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Flatten, Dense
 from tensorflow.keras.layers import Conv2D, BatchNormalization, LeakyReLU, add
 
-goban_size = 9
-nfilters = 256
-nresiduals = 2 # 19 in the original paper.
 
-x = Input((goban_size, goban_size, 17))
+def model(goban_size=19, nfilters=256, nresiduals=19):
+    """AlphaZero model.
 
-# Residual tower: a block followed by 19 residual blocks.
-# First block
-x = Conv2D(nfilters, (3, 3), (1, 1))(x)
-x = BatchNormalization()(x)
-x = LeakyReLU()(x)
+    The defaults are the values from the paper.
+    Architecture:
+    - body
+        - single convolutional block
+        - nresiduals residual blocks
+    - two heads
+        - policy
+        - value
+    """
+    x = Input((goban_size, goban_size, 17))
 
-# Residual blocks
-for _ in range(nresiduals):
-    tmp = Conv2D(nfilters, (3, 3), (1, 1))(x)
-    tmp = BatchNormalization()(tmp)
-    tmp = LeakyReLU()(tmp)
-    tmp = Conv2D(nfilters, (3, 3), (1, 1))(tmp)
-    tmp = BatchNormalization()(tmp)
-    x = add(x, tmp)
+    # First block
+    x = Conv2D(nfilters, (3, 3), (1, 1))(x)
+    x = BatchNormalization()(x)
     x = LeakyReLU()(x)
 
-# Two heads.
-# Policy head
-p = Conv2D(2, (1, 1), (1, 1))(x)
-p = BatchNormalization()(p)
-p = LeakyReLU()(p)
-p = Flatten()(p)
-p = Dense(goban_size * goban_size + 1)(p)
+    # Residual blocks
+    for _ in range(nresiduals):
+        tmp = Conv2D(nfilters, (3, 3), (1, 1))(x)
+        tmp = BatchNormalization()(tmp)
+        tmp = LeakyReLU()(tmp)
+        tmp = Conv2D(nfilters, (3, 3), (1, 1))(tmp)
+        tmp = BatchNormalization()(tmp)
+        x = add(x, tmp)
+        x = LeakyReLU()(x)
 
-# Value head
-v = Conv2D(1, (1, 1), (1, 1))(x)
-v = BatchNormalization()(v)
-v = LeakyReLU()(v)
-v = Flatten()(v)
-v = Dense(nfilters)(v)
-v = LeakyReLU()(v)
-v = Dense(1, activation='tanh')(v)
+    # Policy head
+    p = Conv2D(2, (1, 1), (1, 1))(x)
+    p = BatchNormalization()(p)
+    p = LeakyReLU()(p)
+    p = Flatten()(p)
+    p = Dense(goban_size * goban_size + 1)(p)
 
-model = Model(inputs=x, outputs=[p, v])
+    # Value head
+    v = Conv2D(1, (1, 1), (1, 1))(x)
+    v = BatchNormalization()(v)
+    v = LeakyReLU()(v)
+    v = Flatten()(v)
+    v = Dense(nfilters)(v)
+    v = LeakyReLU()(v)
+    v = Dense(1, activation='tanh')(v)
+
+    return Model(inputs=x, outputs=[p, v])
