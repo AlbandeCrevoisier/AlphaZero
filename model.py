d3be0837e521f035351425a66127123f12c8f4e3
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-05-05 17:06:41 +0200

    Correct model bugs.

--- model.py
+++ model.py
@@ -3,49 +3,49 @@ from tensorflow.keras.layers import Input, Flatten, Dense
 from tensorflow.keras.layers import Conv2D, BatchNormalization, LeakyReLU, add
 
 
-def model(goban_size=19, nfilters=256, nresiduals=19):
+def model(goban_size=19, num_filters=256, num_residuals=19):
     """AlphaZero model.
 
     The defaults are the values from the paper.
     Architecture:
     - body
         - single convolutional block
-        - nresiduals residual blocks
+        - num_residuals residual blocks
     - two heads
         - policy
         - value
     """
-    x = Input((goban_size, goban_size, 17))
+    input = Input((goban_size, goban_size, 17))
 
     # First block
-    x = Conv2D(nfilters, (3, 3), (1, 1))(x)
+    x = Conv2D(num_filters, (3, 3), (1, 1), padding='same')(input)
     x = BatchNormalization()(x)
     x = LeakyReLU()(x)
 
     # Residual blocks
-    for _ in range(nresiduals):
-        tmp = Conv2D(nfilters, (3, 3), (1, 1))(x)
+    for _ in range(num_residuals):
+        tmp = Conv2D(num_filters, (3, 3), (1, 1), padding='same')(x)
         tmp = BatchNormalization()(tmp)
         tmp = LeakyReLU()(tmp)
-        tmp = Conv2D(nfilters, (3, 3), (1, 1))(tmp)
+        tmp = Conv2D(num_filters, (3, 3), (1, 1), padding='same')(x)
         tmp = BatchNormalization()(tmp)
-        x = add(x, tmp)
+        x = add([x, tmp])
         x = LeakyReLU()(x)
 
     # Policy head
-    p = Conv2D(2, (1, 1), (1, 1))(x)
+    p = Conv2D(2, (1, 1), (1, 1), padding='same')(x)
     p = BatchNormalization()(p)
     p = LeakyReLU()(p)
     p = Flatten()(p)
     p = Dense(goban_size * goban_size + 1)(p)
 
     # Value head
-    v = Conv2D(1, (1, 1), (1, 1))(x)
+    v = Conv2D(1, (1, 1), (1, 1), padding='same')(x)
     v = BatchNormalization()(v)
     v = LeakyReLU()(v)
     v = Flatten()(v)
-    v = Dense(nfilters)(v)
+    v = Dense(num_filters)(v)
     v = LeakyReLU()(v)
     v = Dense(1, activation='tanh')(v)
 
-    return Model(inputs=x, outputs=[p, v])
+    return Model(inputs=input, outputs=[p, v])
