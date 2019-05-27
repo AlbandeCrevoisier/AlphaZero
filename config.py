76e1e6be49ac9f4bd69c077e7b543302347ff8f1
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-05-27 21:08:12 +0200

    Add params to config.

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 config = {
+    # Model
     'batch_size': 4096,
     'goban_size': 19,
     'nfilters': 256,
     'nresiduals': 19,
-    'l2_param': 1e-4
+    'l2_param': 1e-4,
+    #MCTS
+    'c_base': 19652,
+    'c_init': 1.25,
+    'nsamples': 30,
+    'dirichlet': 0.03,
+    'explo_fraction': 0.25,
+    'nsim': 800
     }
