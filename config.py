0ee5361fbe448299f905adfe7669099f64621b34
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-06-04 01:25:25 +0200

    Make a reasonable test config.

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 config = {
-    # Model
-    'batch_size': 4096,
-    'goban_size': 19,
-    'nfilters': 256,
-    'nresiduals': 19,
-    'l2_param': 1e-4,
-    #MCTS
-    'c_base': 19652,
-    'c_init': 1.25,
-    'nsamples': 30,
-    'dirichlet': 0.03,
-    'explo_fraction': 0.25,
-    'nsim': 800
+    # Model                      Original Paper values
+    'batch_size': 3,        # 4096
+    'goban_size': 5,          # 19
+    'nfilters': 3,           # 256
+    'nresiduals': 3,          # 19
+    'l2_param': 1e-4,          # 1e-4
+    # MCTS
+    'c_base': 19652,           # 19652
+    'c_init': 1.25,            # 1.25
+    'nsamples': 3,            # 30
+    'dirichlet': 0.03,         # 0.03
+    'explo_fraction': 0.25,    # 0.25
+    'nsim': 800                # 800
     }
