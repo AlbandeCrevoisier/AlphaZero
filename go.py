6689bbfd522b3ee9905e56419b964510e5deb3c0
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-06-18 17:40:05 +0200

    Import minigo Go engine.

--- go.py
+++ go.py
@@ -1,9 +1,22 @@
 import numpy as np
+import minigo_go
+
+
+def terminal(self):
+    pass
+
+
+def terminal_value(self, to_play):
+    pass
+
+
+def is_suicide(action, position):
+    pass
 
 
 def legal_actions(position, prev_position):
     pass
 
 
-def apply(action, history):
+def apply(action, position):
     pass
