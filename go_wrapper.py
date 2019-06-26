2abba36af315d489c42c23a7db92b50ee0980868
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-06-26 20:21:36 +0200

    Add turn to legal_actions parameters.

--- go_wrapper.py
+++ go_wrapper.py
@@ -10,13 +10,13 @@ def terminal_value(self, to_play):
     pass
 
 
-def legal_actions(position, prev_position):
+def legal_actions(to_play, position, prev_position):
     """ Return all legal moves as flattened coordinates. """
     p = go.Position(board=position.copy())
     moves = p.all_legal_moves()
     # brut force ko finding
     for m in moves:
-        if (apply(m, position) == prev_position).all() == True:
+        if (apply(m, to_play, position) == prev_position).all() == True:
             moves.remove(m)
             break
     return moves
