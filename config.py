b8b57f764f95ebdbf85ea51db8e7e525fed00f72
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-07-01 15:31:34 +0200

    Configure max number of moves for self-play.

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 config = {
     # Model                      Original Paper values
-    'batch_size': 3,        # 4096
-    'goban_size': 5,          # 19
-    'nfilters': 3,           # 256
-    'nresiduals': 3,          # 19
-    'l2_param': 1e-4,          # 1e-4
+    'batch_size': 3,             # 4096
+    'goban_size': 5,             # 19
+    'nfilters': 3,               # 256
+    'nresiduals': 3,             # 19
+    'l2_param': 1e-4,            # 1e-4
     # MCTS
-    'c_base': 19652,           # 19652
-    'c_init': 1.25,            # 1.25
-    'nsamples': 3,            # 30
-    'dirichlet': 0.03,         # 0.03
-    'explo_fraction': 0.25,    # 0.25
-    'nsim': 10                # 800
+    'c_base': 19652,             # 19652
+    'c_init': 1.25,              # 1.25
+    'nsamples': 3,               # 30
+    'dirichlet': 0.03,           # 0.03
+    'explo_fraction': 0.25,      # 0.25
+    'nsim': 10,                  # 800
+    'maxmoves': 52               # 722
     }
