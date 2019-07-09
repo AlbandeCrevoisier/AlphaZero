21f1440210b82ec0396d1e80da4abf7ce306a221
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-07-09 21:23:07 +0200

    Update test.

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
+#!/usr/bin/python
+import numpy as np
 from mcts import *
 from model import *
 from config import *
+from go import *
+from go_wrapper import *
 
 c = config
-h = []
+h = [np.zeros((5, 5))]
 m = model(c)
 compile(m)
-m.predict()
-a, r = mcts(c, h, m)
+to_play = 1
+
+for _ in range(50):
+    a, _ = mcts(c, h.copy(), m)
+    h.append(apply(a, to_play, h[-1]))
+    print(type(h[-1]))
+    to_play *= -1
+
+print(h[-1])
+print(Position(h[-1]).score())
