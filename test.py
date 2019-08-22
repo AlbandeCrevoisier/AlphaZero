b5d8075107422bf80fbb1c995e72fd88fa0eaaf0
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-08-22 21:28:46 +0200

    Use more explicit variable names in test.

--- test.py
+++ test.py
@@ -5,24 +5,22 @@ from config import *
 from go import *
 from go_wrapper import *
 
-c = config
 h = [np.zeros((5, 5))]
-cvisits = [[]]
-m = model(c)
+child_visits = []
+t = []
+m = model(config)
 compile(m)
 to_play = 1
 
-for _ in range(50):
-    a, r = mcts(c, h, m)
-    apply(a, h)
-    svisits = sum(c.nvisits for c in r.children.values())
-    v = [r.children[a].nvisits / svisits if a in r.children else 0
+for _ in range(100):
+    action, root = mcts(config, h, m)
+    apply(action, h)
+    sum_visits = sum(c.nvisits for c in root.children.values())
+    visits = [root.children[a].nvisits / sum_visits if a in root.children else 0
         for a in range(26)]
-    print(v)
-    cvisits.append(v)
-
-print(h[-1])
-print("Score: ", Position(h[-1], komi=0).score())
-
-print(cvisits[-1])
-print(len(cvisits), len(h))
+    child_visits.append(visits)
+    # A komi of 3.5 forces B to look for the best possible play.
+    # http://www.mathpuzzle.com/go.html
+    target = (visits, Position(h[-1], komi=3.5).result())
+    print(target)
+    t.append(target)
