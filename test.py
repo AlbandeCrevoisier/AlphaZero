1dababf1fa483702e9256c88504b3e164737c075
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-08-22 21:57:35 +0200

    Add network training to the test-which-is-not-a-test-anymore.

--- test.py
+++ test.py
@@ -7,12 +7,14 @@ from go_wrapper import *
 
 h = [np.zeros((5, 5))]
 child_visits = []
-t = []
+img = []
+target_policies = []
+target_values = []
 m = model(config)
 compile(m)
 to_play = 1
 
-for _ in range(100):
+for i in range(100):
     action, root = mcts(config, h, m)
     apply(action, h)
     sum_visits = sum(c.nvisits for c in root.children.values())
@@ -21,6 +23,8 @@ for _ in range(100):
     child_visits.append(visits)
     # A komi of 3.5 forces B to look for the best possible play.
     # http://www.mathpuzzle.com/go.html
-    target = (visits, Position(h[-1], komi=3.5).result())
-    print(target)
-    t.append(target)
+    target_policies.append(visits)
+    target_values.append(Position(h[-1], komi=3.5).result())
+    img.append(get_input_features(config, h, -1))
+
+m.fit(np.array(img), [target_policies, target_values], epochs=config['nsteps'])
