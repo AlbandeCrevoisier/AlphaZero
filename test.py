78608b6d4df4a9107e65f69bd0b22e2065ecb0a8
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-08-22 21:03:12 +0200

    Add search statistics to test. This is becoming a self_play file, soon to be training too...

--- test.py
+++ test.py
@@ -7,13 +7,22 @@ from go_wrapper import *
 
 c = config
 h = [np.zeros((5, 5))]
+cvisits = [[]]
 m = model(c)
 compile(m)
 to_play = 1
 
 for _ in range(50):
-    a, _ = mcts(c, h, m)
+    a, r = mcts(c, h, m)
     apply(a, h)
+    svisits = sum(c.nvisits for c in r.children.values())
+    v = [r.children[a].nvisits / svisits if a in r.children else 0
+        for a in range(26)]
+    print(v)
+    cvisits.append(v)
 
 print(h[-1])
-print(Position(h[-1], komi=0).score())
+print("Score: ", Position(h[-1], komi=0).score())
+
+print(cvisits[-1])
+print(len(cvisits), len(h))
